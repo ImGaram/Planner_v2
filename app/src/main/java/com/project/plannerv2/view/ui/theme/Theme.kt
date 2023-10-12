87e3e3a1ad421ea149bf882ec3379c09c01d4851
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-10-12 09:44:12 +0900

    :art: :: 패키지 구조 세팅

--- app/src/main/java/com/project/plannerv2/view/ui/theme/Theme.kt
+++ app/src/main/java/com/project/plannerv2/view/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.project.plannerv2.ui.theme
+package com.project.plannerv2.view.ui.theme
 
 import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/project/plannerv2/view/ui/theme/Type.kt
+++ app/src/main/java/com/project/plannerv2/view/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.project.plannerv2.ui.theme
+package com.project.plannerv2.view.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
