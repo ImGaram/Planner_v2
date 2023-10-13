da16705dc9f9848267966263aed0436ab806e428
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-10-14 00:45:16 +0900

    :lipstick: :: bottom navigation bar 적용

--- app/src/main/java/com/project/plannerv2/view/MainActivity.kt
+++ app/src/main/java/com/project/plannerv2/view/MainActivity.kt
@@ -3,21 +3,43 @@ package com.project.plannerv2.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.project.plannerv2.view.login.navigation.loginRoute
+import com.project.plannerv2.view.component.bottomnavigation.PlannerV2BottomNavigation
+import com.project.plannerv2.view.plan.navigation.planRoute
 import com.project.plannerv2.view.ui.theme.PlannerV2Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PlannerV2Theme {
-                PlannerV2NavHost(
-                    navHostController = rememberNavController(),
-                    startDestination = loginRoute
-                )
+                val navHostController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        PlannerV2BottomNavigation(navHostController = navHostController)
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it)
+                    ) {
+                        PlannerV2NavHost(
+                            navHostController = navHostController,
+                            startDestination = planRoute
+                        )
+                    }
+                }
             }
         }
     }
