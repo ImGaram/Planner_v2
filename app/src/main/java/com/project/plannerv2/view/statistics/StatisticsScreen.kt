a89a88a8d0d9fcd483bf57f449cbfe08b5f3438e
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-10-13 22:20:50 +0900

    :lipstick: :: 빈 screen 생성

---
+++ app/src/main/java/com/project/plannerv2/view/statistics/StatisticsScreen.kt
@@ -0,0 +1,15 @@
+package com.project.plannerv2.view.statistics
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StatisticsScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatisticsScreenPreview() {
+    StatisticsScreen()
+}
