78ade9f984f3cdec1dc5f3095eaa6834b94d0630
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-10-14 00:44:19 +0900

    :sparkles: :: bottom navigation item 생성

--- app/src/main/java/com/project/plannerv2/view/component/bottomnavigation/BottomNavItem.kt
+++ app/src/main/java/com/project/plannerv2/view/component/bottomnavigation/BottomNavItem.kt
@@ -1,5 +1,12 @@
 package com.project.plannerv2.view.component.bottomnavigation
 
-sealed class BottomNavItem {
+import com.project.plannerv2.R
+import com.project.plannerv2.view.plan.navigation.planRoute
+import com.project.plannerv2.view.statistics.navigation.statisticsRoute
 
+sealed class BottomNavItem(
+    val icon: Int, val route: String
+) {
+    object Calendar: BottomNavItem(R.drawable.ic_calendar, planRoute)
+    object Statistics: BottomNavItem(R.drawable.ic_statistics, statisticsRoute)
 }
