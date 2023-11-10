093c23ed06a99672d08af969ede6e4ddae468fc1
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-11-10 14:49:56 +0900

    :sparkles: :: auto login logic 구현

--- app/src/main/java/com/project/plannerv2/view/MainActivity.kt
+++ app/src/main/java/com/project/plannerv2/view/MainActivity.kt
@@ -3,12 +3,14 @@ package com.project.plannerv2.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.project.plannerv2.view.component.bottomnavigation.PlannerV2BottomNavigation
@@ -17,10 +19,19 @@ import com.project.plannerv2.view.login.navigation.loginRoute
 import com.project.plannerv2.view.plan.navigation.planRoute
 import com.project.plannerv2.view.statistics.navigation.statisticsRoute
 import com.project.plannerv2.view.ui.theme.PlannerV2Theme
+import com.project.plannerv2.viewmodel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+    private val splashViewModel by viewModels<SplashViewModel>()
+    private val loginState: Boolean? by lazy { splashViewModel.loginState.value }
+    private val splashState: Boolean by lazy { splashViewModel.splashState.value }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        splashViewModel.checkLogin()
+        installSplashScreen().setKeepOnScreenCondition { splashState }
+
         setContent {
             PlannerV2Theme {
                 val navHostController = rememberNavController()
@@ -41,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         PlannerV2NavHost(
                             navHostController = navHostController,
-                            startDestination = loginRoute
+                            startDestination = if (loginState!!) planRoute else loginRoute
                         )
                     }
                 }
