da1f5249b993ffbe6734c2858e3f3762bb25b6bd
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-11-12 16:31:05 +0900

    :sparkles: :: plan data 일정 완료 여부 데이터 생성

--- app/src/main/java/com/project/plannerv2/data/PlanData.kt
+++ app/src/main/java/com/project/plannerv2/data/PlanData.kt
@@ -1,6 +1,7 @@
 package com.project.plannerv2.data
 
 data class PlanData(
-    val title: String,
-    val description: String
+    val title: String = "",
+    val description: String = "",
+    val complete: Boolean = false
 )
