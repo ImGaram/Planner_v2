e12ca81f0c6715f4cc567df15f5bb1c21f68b5b6
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-11-29 08:50:26 +0900

    :fire: :: 주간 일정 통계 불러오는 로직 적용

--- app/src/main/java/com/project/plannerv2/view/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/project/plannerv2/view/statistics/StatisticsScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -18,13 +20,23 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.project.plannerv2.view.component.progress.CircularProgressScreen
 import com.project.plannerv2.view.statistics.component.ThisWeekPlanStatisticsChart
 import com.project.plannerv2.view.statistics.component.WeeklyCompletionStatisticsChart
+import com.project.plannerv2.viewmodel.StatisticsViewModel
 import kotlin.math.roundToInt
 
 @Composable
-fun StatisticsScreen() {
+fun StatisticsScreen(statisticsViewModel: StatisticsViewModel = viewModel()) {
+    val thisWeekStatisticsState = statisticsViewModel.thisWeekStatistics.collectAsState().value
     val scrollState = rememberScrollState()
+
+    val uid = FirebaseAuth.getInstance().uid
+    LaunchedEffect(Unit) {
+        statisticsViewModel.getThisWeekStatistics(uid!!)
+    }
 
     Column(
         modifier = Modifier
@@ -45,29 +57,32 @@ fun StatisticsScreen() {
         )
 
         // firebase data 로 변경
-        val totalPlanCount = 51
-        val completedPlanCount = 46
-        ThisWeekPlanStatisticsChart(
-            totalPlanCount = totalPlanCount,
-            completedPlanCount = completedPlanCount
-        )
+        val totalPlanCount = thisWeekStatisticsState?.total
+        val completedPlanCount = thisWeekStatisticsState?.completed
 
-        Divider(
-            modifier = Modifier
-                .height(1.dp)
-                .padding(horizontal = 15.dp)
-        )
+        if (totalPlanCount != null && completedPlanCount != null) {
+            ThisWeekPlanStatisticsChart(
+                totalPlanCount = totalPlanCount,
+                completedPlanCount = completedPlanCount
+            )
 
-        val completionRate = completedPlanCount.toFloat() / totalPlanCount.toFloat() * 100f
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(15.dp)
-                .background(Color(0xCCFFF6A7)),
-            text = "이번 주 일정 완료율은 ${(completionRate * 10.0).roundToInt() / 10.0}% 입니다.",
-            fontSize = 18.sp,
-            textAlign = TextAlign.Center
-        )
+            Divider(
+                modifier = Modifier
+                    .height(1.dp)
+                    .padding(horizontal = 15.dp)
+            )
+
+            val completionRate = completedPlanCount.toFloat() / totalPlanCount.toFloat() * 100f
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
+                    .background(Color(0xCCFFF6A7)),
+                text = "이번 주 일정 완료율은 ${(completionRate * 10.0).roundToInt() / 10.0}% 입니다.",
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        } else CircularProgressScreen()
 
         Text(
             modifier = Modifier.padding(15.dp),
