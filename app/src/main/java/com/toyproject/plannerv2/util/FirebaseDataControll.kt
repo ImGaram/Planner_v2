68bb51e0a340ae8046b2024f1cde9ea164e6a737
ImGaram <gim78014@gmail.com>
2024-04-09 20:39:55 +0900

    :sparkles: :: 일정 crud 함수 생성(#41)

---
+++ app/src/main/java/com/toyproject/plannerv2/util/FirebaseDataControll.kt
@@ -0,0 +1,64 @@
+package com.toyproject.plannerv2.util
+
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+
+fun DocumentReference.createFireStoreData(
+    setValue: Any,
+    onSuccess: () -> Unit = {},
+    onFailure: (Exception?) -> Unit = {},
+) {
+    this.set(setValue).addOnCompleteListener { task ->
+        if (task.isSuccessful) onSuccess()
+        else onFailure(task.exception)
+    }
+}
+
+fun CollectionReference.readFireStoreData(
+    onSuccess: (List<DocumentSnapshot>) -> Unit = {},
+    onFailure: (Exception?) -> Unit = {}
+) {
+    this.get().addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            val querySnapshot = task.result
+            onSuccess(querySnapshot.documents)
+        } else onFailure(task.exception)
+    }
+}
+
+fun DocumentReference.readFireStoreData(
+    onSuccess: (DocumentSnapshot) -> Unit = {},
+    onFailure: (Exception?) -> Unit = {}
+) {
+    this.get().addOnCompleteListener { task ->
+        if (task.isSuccessful) onSuccess(task.result)
+        else onFailure(task.exception)
+    }
+}
+
+fun Query.readFireStoreData(
+    onSuccess: (List<DocumentSnapshot>) -> Unit = {},
+    onFailure: (Exception?) -> Unit = {}
+) {
+    this.get().addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            val querySnapshot = task.result
+            onSuccess(querySnapshot.documents)
+        } else onFailure(task.exception)
+    }
+}
+
+fun DocumentReference.updateFireStoreData(
+    updateValue: Map<String, Any>,
+    onSuccess: () -> Unit = {},
+    onFailure: (Exception?) -> Unit = {}
+) {
+    this.update(updateValue).addOnCompleteListener { task ->
+        if (task.isSuccessful) onSuccess()
+        else onFailure(task.exception)
+    }
+}
+
+// delete 만들기(필요하면)
