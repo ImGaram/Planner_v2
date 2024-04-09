3e088c7473190ef3da2283b5c60b2e95d29d69b1
ImGaram <gim78014@gmail.com>
2024-04-10 01:02:41 +0900

    :sparkles: :: 주간 일정 구하는 함수 데이터베이스 코드 변경.(#40)

--- app/src/main/java/com/toyproject/plannerv2/viewmodel/StatisticsViewModel.kt
+++ app/src/main/java/com/toyproject/plannerv2/viewmodel/StatisticsViewModel.kt
@@ -3,36 +3,54 @@ package com.toyproject.plannerv2.viewmodel
 import androidx.lifecycle.ViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import com.toyproject.plannerv2.data.PlanData
 import com.toyproject.plannerv2.data.StatisticsData
 import com.toyproject.plannerv2.util.StatisticsMode
+import com.toyproject.plannerv2.util.readFireStoreData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.temporal.TemporalAdjusters
 
 class StatisticsViewModel: ViewModel() {
-    // 통계 데이터베이스 로직
-    // 처음에는 없겠죠?
-    // 처음에 일정을 생성할 때, total을 처음에 생성한 데이터 수로 정함
-    // 갈수록 +하기
-    // 삭제하면(추후에 추가 예정) -하기
-
     private val _thisWeekStatistics = MutableStateFlow<StatisticsData?>(null)
     val thisWeekStatistics = _thisWeekStatistics.asStateFlow()
 
+    // 주간 일정을 불러온다.
     fun getThisWeekStatistics(uid: String) {
-//        val thisWeekReference = FirebaseDatabase.getInstance().reference
-//            .child("statistics")
-//            .child(uid)
-//            .child("this week")
-//
-//        thisWeekReference.getFirebaseData(
-//            onDataChangeLogic = { snapshot ->
-//                val statisticsData = snapshot.getValue(StatisticsData::class.java)
-//
-//                if (statisticsData != null) {
-//                    _thisWeekStatistics.value = statisticsData
-//                }
-//            }
-//        )
+        val getThisWeekRef = FirebaseFirestore.getInstance()
+            .collection("schedule")
+            .document(uid)
+            .collection("plans")
+
+        val today = LocalDate.now()
+        // 주의 첫날은 일요일, 주의 마지막 날은 토요일
+        val firstDayOfWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY))
+        val lastDayOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY))
+        // second가 아닌 millisecond의 timestmp를 받기 위함
+        val timestampFirstDay = firstDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000
+        val timestampLastDay = lastDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000
+
+        getThisWeekRef.whereGreaterThan("createdTime", timestampFirstDay)
+            .whereLessThan("createdTime", timestampLastDay)
+            .readFireStoreData(
+                onSuccess = {
+                    var completedPlansCount = 0
+                    it.forEach { documentSnapshot ->
+                        val documentData = documentSnapshot.toObject<PlanData>()
+                        if (documentData?.complete == true) completedPlansCount++
+                    }
+                    // 주간 일정에서 일정의 총 개수와 완료한 일정 개수를 저장함.
+                    _thisWeekStatistics.value = StatisticsData(
+                        total = it.size,
+                        completed = completedPlansCount
+                    )
+                }
+            )
     }
 
     private fun modifyTotalData(
