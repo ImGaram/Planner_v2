3652f195e08eb6b03160c303dfa9eded29ac073e
ImGaram <gim78014@gmail.com>
2024-04-09 20:38:33 +0900

    :sparkles: :: planData baseDate와 createdTime 추가(#41)

--- app/src/main/java/com/toyproject/plannerv2/data/PlanData.kt
+++ app/src/main/java/com/toyproject/plannerv2/data/PlanData.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
 class PlanData(
+    val baseDate: String = "",
     val title: String = "",
     val description: String = "",
+    val createdTime: Long? = null,
     complete: Boolean = false
 ) {
     var complete by mutableStateOf(complete)
