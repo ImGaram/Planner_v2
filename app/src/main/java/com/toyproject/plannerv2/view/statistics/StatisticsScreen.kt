b6b8527719c05e23e0331c7768de3e90d7ffe04d
ImGaram <gim78014@gmail.com>
2024-04-12 01:47:01 +0900

    :sparkles: :: 일간 차트 적용(#40)

--- app/src/main/java/com/toyproject/plannerv2/view/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/statistics/StatisticsScreen.kt
@@ -1,9 +1,7 @@
 package com.toyproject.plannerv2.view.statistics
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -14,9 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,16 +22,17 @@ import com.toyproject.plannerv2.view.component.progress.CircularProgressScreen
 import com.toyproject.plannerv2.view.statistics.component.ThisWeekPlanStatisticsChart
 import com.toyproject.plannerv2.view.statistics.component.WeeklyCompletionStatisticsChart
 import com.toyproject.plannerv2.viewmodel.StatisticsViewModel
-import kotlin.math.roundToInt
 
+// todo :: 기존 차트(이번 주 일정 차트)를 일간 일정 통계(완료한 일정만 카운트)로 변경하기, 기존 차트 데이터는 스코어카드 형식으로 빼기.
+// todo :: 있으면 좋을 것 같은 UI 요소: 연속된 일정 생성 카운트
 @Composable
 fun StatisticsScreen(statisticsViewModel: StatisticsViewModel = viewModel()) {
-    val thisWeekStatisticsState = statisticsViewModel.thisWeekStatistics.collectAsState()
+    val dailyStatisticsState = statisticsViewModel.dailyStatistics.collectAsState()
     val scrollState = rememberScrollState()
 
     val uid = FirebaseAuth.getInstance().uid
     LaunchedEffect(Unit) {
-        statisticsViewModel.getThisWeekStatistics(uid!!)
+        statisticsViewModel.getDailyStatistics(uid!!)
     }
 
     Column(
@@ -45,7 +42,7 @@ fun StatisticsScreen(statisticsViewModel: StatisticsViewModel = viewModel()) {
     ) {
         Text(
             modifier = Modifier.padding(15.dp),
-            text = "이번 주 일정 통계",
+            text = "일간 일정 통계",
             fontSize = 22.sp,
             fontWeight = FontWeight.SemiBold
         )
@@ -56,34 +53,9 @@ fun StatisticsScreen(statisticsViewModel: StatisticsViewModel = viewModel()) {
                 .padding(horizontal = 15.dp)
         )
 
-        // firebase data 로 변경
-        val totalPlanCount = thisWeekStatisticsState.value?.total
-        val completedPlanCount = thisWeekStatisticsState.value?.completed
-
-        if (totalPlanCount != null && completedPlanCount != null) {
+        if (dailyStatisticsState.value.isNotEmpty()) {
             ThisWeekPlanStatisticsChart(
-                totalPlanCount = totalPlanCount,
-                completedPlanCount = completedPlanCount
-            )
-
-            Divider(
-                modifier = Modifier
-                    .height(1.dp)
-                    .padding(horizontal = 15.dp)
-            )
-
-            // 일정 완료율 계산
-            val completionRate = completedPlanCount.toFloat() / totalPlanCount.toFloat() * 100f
-            // 완료율이 not a number(NaN)일 때 0으로 처리.
-            val completionRateDecimal = ((if (completionRate.isNaN()) 0f else completionRate) * 10.0).roundToInt() / 10.0
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp)
-                    .background(Color(0xCCFFF6A7)),
-                text = "이번 주 일정 완료율은 ${completionRateDecimal}% 입니다.",
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
+                dailyPlanList = dailyStatisticsState.value.toList()
             )
         } else CircularProgressScreen()
 
@@ -101,7 +73,7 @@ fun StatisticsScreen(statisticsViewModel: StatisticsViewModel = viewModel()) {
         )
 
         // firebase data 로 변경
-        val completedPlanList = listOf(65, 23, 77, 136, 55)
+        val completedPlanList = listOf(65, 23, 77, 146, 55)
         val completedRateList = listOf(100, 64, 89, 96, 77)
         WeeklyCompletionStatisticsChart(
             completedPlanList = completedPlanList,
