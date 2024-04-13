1b4f2b905c415bc91c2c412a208a4cf5046719dd
ImGaram <gim78014@gmail.com>
2024-04-13 17:21:47 +0900

    :sparkles: :: 날짜 형식 변경 공통 함수 생성(#40)

--- app/src/main/java/com/toyproject/plannerv2/util/Util.kt
+++ app/src/main/java/com/toyproject/plannerv2/util/Util.kt
@@ -2,6 +2,10 @@ package com.toyproject.plannerv2.util
 
 import com.patrykandpatrick.vico.core.entry.FloatEntry
 import com.patrykandpatrick.vico.core.entry.entryOf
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 // 여러 곳에서 사용되는 함수(공용 함수)만 모아놓은 곳.
 
@@ -15,4 +19,13 @@ fun intListAsFloatEntryList(list: List<Int>): List<FloatEntry> {
     }
 
     return floatEntryList
+}
+
+fun String.stringToUnixTimestamp(): Long {
+    val localDate = LocalDate.parse(this, DateTimeFormatter.ISO_DATE)
+    return localDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000
+}
+
+fun Long.unixTimestampToLocalDate(): LocalDate {
+    return Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDate()
 }
