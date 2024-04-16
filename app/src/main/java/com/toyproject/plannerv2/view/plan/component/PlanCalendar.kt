8db9aee16bd6bf2a5c94d9188b121abca3144292
ImGaram <gim78014@gmail.com>
2024-04-16 23:41:37 +0900

    :sparkles: :: 기존 dataStore 코드 제거, selectedDate 받아올 수 있도록 변경(#48)

--- app/src/main/java/com/toyproject/plannerv2/view/plan/component/PlanCalendar.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/plan/component/PlanCalendar.kt
@@ -3,43 +3,26 @@ package com.toyproject.plannerv2.view.plan.component
 import android.widget.CalendarView
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import com.toyproject.plannerv2.util.DateDataStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Locale
+import java.time.LocalDate
 
 @Composable
 fun PlanCalendar(
-    date: MutableState<String?>,
     scope: CoroutineScope,
-    dataStore: DateDataStore,
-    onDateStateChange: () -> Unit,
+    onDateStateChange: (String) -> Unit,
 ) {
     AndroidView(
         modifier = Modifier.fillMaxWidth(),
         factory = { CalendarView(it) }
     ) { calendarView ->
-        val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
-        if (!date.value.isNullOrEmpty())
-            calendarView.date = formatter.parse(date.value!!)!!.time
-
         calendarView.setOnDateChangeListener { _, year, month, day ->
             scope.launch {
-                date.value = initSelectDate(year, month+1, day)
-                dataStore.setDate(initSelectDate(year, month+1, day))
-                onDateStateChange()
+                val selectedDate = LocalDate.of(year, month+1, day)
+                onDateStateChange(selectedDate.toString())
             }
         }
     }
-}
-
-private fun initSelectDate(year: Int, month: Int, day: Int): String {
-    val initMonth = if (month < 10) "0$month" else month.toString()
-    val initDay = if (day < 10) "0$day" else day.toString()
-
-    return "$year-$initMonth-$initDay"
 }
