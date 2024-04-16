4f1e8740da610a1410b6caf7d5063acb2e6b2f75
ImGaram <gim78014@gmail.com>
2024-04-16 23:40:10 +0900

    :sparkles: :: createPlan 화면에서 selectedDate 받을 수 있도록 하는 코드(#48)

--- app/src/main/java/com/toyproject/plannerv2/view/create/navigation/CreatePlanNavigation.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/create/navigation/CreatePlanNavigation.kt
@@ -2,17 +2,28 @@ package com.toyproject.plannerv2.view.create.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.toyproject.plannerv2.view.create.CreatePlanScreen
 
 const val createPlanRoute = "createPlan"
 
-fun NavHostController.navigateToCreatePlan() {
-    this.navigate(createPlanRoute)
+fun NavHostController.navigateToCreatePlan(selectedDate: String) {
+    this.navigate("$createPlanRoute/$selectedDate")
 }
 
 fun NavGraphBuilder.createPlanScreen(navigateToPlan: () -> Unit) {
-    composable(route = createPlanRoute) {
-        CreatePlanScreen(navigateToPlan = navigateToPlan)
+    composable(
+        route = "$createPlanRoute/{selectedDate}",
+        arguments = listOf(
+            navArgument("selectedDate") { type = NavType.StringType }
+        )
+    ) {
+        val selectedDate = it.arguments?.getString("selectedDate")
+        CreatePlanScreen(
+            selectedDate = selectedDate,
+            navigateToPlan = navigateToPlan
+        )
     }
 }
