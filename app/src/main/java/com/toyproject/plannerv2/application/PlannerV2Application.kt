c81e4342073c4c545490f9583cd0b8327bd37d2a
ImGaram <gim78014@gmail.com>
2024-04-17 00:30:34 +0900

    :fire: :: application 코드 제거

--- app/src/main/java/com/toyproject/plannerv2/application/PlannerV2Application.kt
+++ app/src/main/java/com/toyproject/plannerv2/application/PlannerV2Application.kt
@@ -2,15 +2,4 @@ package com.toyproject.plannerv2.application
 
 import android.app.Application
 
-class PlannerV2Application: Application() {
-    companion object {
-        private lateinit var plannerV2Application: PlannerV2Application
-        fun getInstance() = plannerV2Application
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        plannerV2Application = this
-    }
-}
+class PlannerV2Application: Application()
