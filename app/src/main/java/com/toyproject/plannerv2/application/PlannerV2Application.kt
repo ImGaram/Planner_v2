219d3df942f18f70a9c615ccf083429ad21f1959
ImGaram <gim78014@gmail.com>
2024-04-16 23:33:48 +0900

    :fire: :: application에서 dataStore 코드 제거(#48)

--- app/src/main/java/com/toyproject/plannerv2/application/PlannerV2Application.kt
+++ app/src/main/java/com/toyproject/plannerv2/application/PlannerV2Application.kt
@@ -1,11 +1,8 @@
 package com.toyproject.plannerv2.application
 
 import android.app.Application
-import com.toyproject.plannerv2.util.DateDataStore
 
 class PlannerV2Application: Application() {
-    private lateinit var dataStore: DateDataStore
-
     companion object {
         private lateinit var plannerV2Application: PlannerV2Application
         fun getInstance() = plannerV2Application
@@ -15,8 +12,5 @@ class PlannerV2Application: Application() {
         super.onCreate()
 
         plannerV2Application = this
-        dataStore = DateDataStore(this)
     }
-
-    fun getDataStore(): DateDataStore = dataStore
 }
