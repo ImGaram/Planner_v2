785072475ea73e27c3472dcf4131bf1e42dbb9e7
ImGaram <gim78014@gmail.com>
2024-04-19 23:11:14 +0900

    :sparkles: :: nvHost에 category 추가(#54)

--- app/src/main/java/com/toyproject/plannerv2/view/MainActivity.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.toyproject.plannerv2.view.category.navigation.categoryRoute
 import com.toyproject.plannerv2.view.component.bottomnavigation.PlannerV2BottomNavigation
 import com.toyproject.plannerv2.view.component.topbar.PlannerV2TopAppBar
 import com.toyproject.plannerv2.view.login.component.LoginLoadingIndicator
@@ -47,7 +48,7 @@ class MainActivity : ComponentActivity() {
                         topBar = { PlannerV2TopAppBar() },
                         bottomBar = {
                             when (currentRoute?.destination?.route) {
-                                planRoute, statisticsRoute -> PlannerV2BottomNavigation(navHostController = navHostController)
+                                planRoute, categoryRoute, statisticsRoute -> PlannerV2BottomNavigation(navHostController = navHostController)
                             }
                         }
                     ) {
--- app/src/main/java/com/toyproject/plannerv2/view/PlannerV2NavHost.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/PlannerV2NavHost.kt
@@ -3,6 +3,7 @@ package com.toyproject.plannerv2.view
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.toyproject.plannerv2.view.category.navigation.categoryScreen
 import com.toyproject.plannerv2.view.create.navigation.createPlanScreen
 import com.toyproject.plannerv2.view.create.navigation.navigateToCreatePlan
 import com.toyproject.plannerv2.view.login.navigation.loginScreen
@@ -29,5 +30,7 @@ fun PlannerV2NavHost(
         )
 
         statisticsScreen()
+
+        categoryScreen()
     }
 }
