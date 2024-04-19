3e5b7e931f6545ff0b809232aa9fb2b69f4512ec
ImGaram <gim78014@gmail.com>
2024-04-19 23:10:52 +0900

    :lipstick: :: bottom navigation에 category, ripple theme 적용(#54)

--- app/src/main/java/com/toyproject/plannerv2/view/component/bottomnavigation/BottomNavItem.kt
+++ app/src/main/java/com/toyproject/plannerv2/view/component/bottomnavigation/BottomNavItem.kt
@@ -1,6 +1,7 @@
 package com.toyproject.plannerv2.view.component.bottomnavigation
 
 import com.toyproject.plannerv2.R
+import com.toyproject.plannerv2.view.category.navigation.categoryRoute
 import com.toyproject.plannerv2.view.plan.navigation.planRoute
 import com.toyproject.plannerv2.view.statistics.navigation.statisticsRoute
 
@@ -8,5 +9,6 @@ sealed class BottomNavItem(
     val icon: Int, val route: String
 ) {
     object Calendar: BottomNavItem(R.drawable.ic_calendar, planRoute)
+    object Category: BottomNavItem(R.drawable.ic_category, categoryRoute)
     object Statistics: BottomNavItem(R.drawable.ic_statistics, statisticsRoute)
 }
