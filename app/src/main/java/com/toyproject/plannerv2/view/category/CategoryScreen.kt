d444c100a15e8847a32c4e4964aebddacc1e6137
ImGaram <gim78014@gmail.com>
2024-04-19 23:08:34 +0900

    :lipstick: :: 카테고리 화면 생성(#54)

---
+++ app/src/main/java/com/toyproject/plannerv2/view/category/CategoryScreen.kt
@@ -0,0 +1,14 @@
+package com.toyproject.plannerv2.view.category
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CategoryScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "category screen")
+    }
+}
