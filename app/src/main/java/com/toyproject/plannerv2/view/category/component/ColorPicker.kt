e251a6c238111c3a2967e0e2c3f48870380de998
ImGaram <gim78014@gmail.com>
2024-04-22 00:53:58 +0900

    :lipstick: :: ColorPicker 생성(#54)

---
+++ app/src/main/java/com/toyproject/plannerv2/view/category/component/ColorPicker.kt
@@ -0,0 +1,21 @@
+package com.toyproject.plannerv2.view.category.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.godaddy.android.colorpicker.ClassicColorPicker
+import com.godaddy.android.colorpicker.HsvColor
+
+@Composable
+fun ColorPicker(
+    modifier: Modifier = Modifier,
+    onColorChange: (color: Color) -> Unit
+) {
+    ClassicColorPicker(
+        modifier = modifier,
+        color = HsvColor.from(Color.White),
+        onColorChanged = { hsvColor: HsvColor ->
+            onColorChange(hsvColor.toColor())
+        }
+    )
+}
