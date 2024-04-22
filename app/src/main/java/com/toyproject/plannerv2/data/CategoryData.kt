f73597d26952cc4400fc94c3682602e1343b7221
ImGaram <gim78014@gmail.com>
2024-04-22 18:23:44 +0900

    :sparkles: :: CategoryData data class 생성(#54)

---
+++ app/src/main/java/com/toyproject/plannerv2/data/CategoryData.kt
@@ -0,0 +1,8 @@
+package com.toyproject.plannerv2.data
+
+data class CategoryData(
+    val categoryTitle: String = "",
+    val savedPlanCount: Int = 0,
+    val categoryColorHex: String = "",
+    val createdTime: String = ""
+)
