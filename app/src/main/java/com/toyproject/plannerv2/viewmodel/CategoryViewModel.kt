64a4a2ab36cbca3df59d9894b37dd1bf1795bec6
ImGaram <gim78014@gmail.com>
2024-04-30 02:17:54 +0900

    :sparkles: :: 일정 불러오는 기능 추가(#54)

--- app/src/main/java/com/toyproject/plannerv2/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/toyproject/plannerv2/viewmodel/CategoryViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import com.toyproject.plannerv2.data.CategoryData
 import com.toyproject.plannerv2.util.createFireStoreData
+import com.toyproject.plannerv2.util.readFireStoreData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class CategoryViewModel: ViewModel() {
+    private val _categories = MutableStateFlow<List<CategoryData>?>(null)
+    val categories = _categories.asStateFlow()
 
     fun createCategory(uid: String, categoryData: CategoryData) {
         val createCategoryRef = FirebaseFirestore.getInstance()
@@ -15,5 +20,26 @@ class CategoryViewModel: ViewModel() {
             .document(categoryData.createdTime.toString())
 
         createCategoryRef.createFireStoreData(setValue = categoryData)
+    }
+
+    fun getCategory(uid: String, onFailure: (Exception?) -> Unit = {}) {
+        val categoryRef = FirebaseFirestore.getInstance()
+            .collection("schedule")
+            .document(uid)
+            .collection("category")
+
+        categoryRef.readFireStoreData(
+            onSuccess = {
+                val categoryList = mutableListOf<CategoryData>()
+                it.forEach { documentSnapshot ->
+                    val categoryData = documentSnapshot.toObject(CategoryData::class.java)
+                    if (categoryData != null) categoryList.add(categoryData)
+                }
+                _categories.value = categoryList
+            },
+            onFailure = {
+                onFailure(it)
+            }
+        )
     }
 }
