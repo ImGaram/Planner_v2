9f5437bfe54792bc8f168cbc06202a906382bc20
ImGaram <gim78014@gmail.com>
2024-05-02 03:53:12 +0900

    :fire: :: 일정 저장 개수 필드 삭제(#54)

--- app/src/main/java/com/toyproject/plannerv2/data/CategoryData.kt
+++ app/src/main/java/com/toyproject/plannerv2/data/CategoryData.kt
@@ -2,7 +2,6 @@ package com.toyproject.plannerv2.data
 
 data class CategoryData(
     val categoryTitle: String = "",
-    val savedPlanCount: Int = 0,
     val categoryColorHex: String = "",
     val createdTime: Long? = null
 )
