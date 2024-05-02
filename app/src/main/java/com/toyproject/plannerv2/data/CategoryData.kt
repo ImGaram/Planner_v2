dd0578ab49d3af59dd91c99bcda5ccf255346f41
ImGaram <gim78014@gmail.com>
2024-05-03 00:18:14 +0900

    :sparkles: :: CategoryData 구분용 id 필드 생성(#54)

--- app/src/main/java/com/toyproject/plannerv2/data/CategoryData.kt
+++ app/src/main/java/com/toyproject/plannerv2/data/CategoryData.kt
@@ -1,6 +1,7 @@
 package com.toyproject.plannerv2.data
 
 data class CategoryData(
+    val id: String = "",
     val categoryTitle: String = "",
     val categoryColorHex: String = "",
     val createdTime: Long? = null
