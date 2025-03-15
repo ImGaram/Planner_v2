35616d5b02b372c327f3eca35030218ef6672e43
ImGaram <gim78014@gmail.com>
2025-03-15 21:07:15 +0900

    :sparkles: :: 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.toyproject.plannerv2"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
